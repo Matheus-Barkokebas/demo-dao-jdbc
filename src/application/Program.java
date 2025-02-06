387989c1ee75dbb454b2cffec3bbe7cb8aa2fe72
Matheus <matheusbarkcaribe@gmail.com>
2025-02-05 22:53:29 -0300

    findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,14 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		
+		System.out.println("##################################");
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
